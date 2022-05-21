18f235fc39de764ff3dfb24ca08d01e9c59aba01
Dimitris Gkantzos <jimaster213@gmail.com>
2022-05-21 13:51:30 +0300

    Adjust app to use react-router and clean up

---
+++ src/Routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
+  );
+}
