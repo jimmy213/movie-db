63cc6c5ac07d52885461fe23be4121f2ccb898ff
Dimitris Gkantzos <jimaster213@gmail.com>
2022-05-21 15:08:42 +0300

    Add basic styles structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./Routes";
 
+import "./styles/application.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
