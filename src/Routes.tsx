8962e609d9307ef49054740e5ec8f90922261fbf
Dimitris Gkantzos <jimaster213@gmail.com>
2022-05-22 17:41:05 +0300

    Implement PageHeader component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageHeader from "./components/PageHeader";
 
 export default function AppRoutes() {
   return (
     <Router>
+      <PageHeader />
+
       <Routes>
         <Route path="/" />
       </Routes>
