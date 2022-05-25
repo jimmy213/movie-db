e065887b5c707c56965af9d6a49f6bfc46f79d71
Dimitris Gkantzos <jimaster213@gmail.com>
2022-05-25 20:49:30 +0300

    Implement Movies page

---
+++ src/pages/movies/index.tsx
@@ -0,0 +1,9 @@
+export const Movies = () => {
+  return (
+    <main className="moviespage">
+      <div className="moviespage-wrapper container">
+        <h2>Movies</h2>
+      </div>
+    </main>
+  );
+};
