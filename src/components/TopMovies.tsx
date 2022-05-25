e2b51833e76d34b0ff08ac38b69ea318ab4505f7
Dimitris Gkantzos <jimaster213@gmail.com>
2022-05-25 13:16:36 +0300

    Adjust TopMovies to use Modal component

--- src/components/TopMovies.tsx
+++ src/components/TopMovies.tsx
@@ -1,19 +1,30 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setCurrentMovieId } from "../globalSlice";
+import { open as _openModal } from "./Modal/modalSlice";
 
 interface TopMoviesProps {
   movieData: Array<any>;
 }
 
 export const TopMovies: FC<TopMoviesProps> = ({ movieData }) => {
+  const dispatch = useDispatch();
+
+  const openModal = (modalName: string, movieId: number) => (event: any) => {
+    event.preventDefault();
+
+    dispatch(setCurrentMovieId(movieId));
+    dispatch(_openModal(modalName));
+  };
+
   return (
     <ul className="top-list">
       {movieData &&
-        movieData.slice(0, 10).map((movie: { id: number; original_title: string }) => (
+        movieData.slice(0, 10).map((movie: { id: number; title: string }) => (
           <li key={movie.id} className="top-list-item">
-            <Link to={`/movies/${movie.id}`} className="movie-link">
-              {movie.original_title}
-            </Link>
+            <button className="movie-link" onClick={openModal("movie-details", movie.id)}>
+              {movie.title}
+            </button>
           </li>
         ))}
     </ul>
