9f41541552d4a066ef85559d7edda68ae0a80194
Dimitris Gkantzos <jimaster213@gmail.com>
2022-05-27 15:48:19 +0300

    Cleanup TopMovies component

--- src/components/TopMovies.tsx
+++ src/components/TopMovies.tsx
@@ -1,30 +1,18 @@
 import { FC } from "react";
-import { useDispatch } from "react-redux";
-import { setCurrentMovieId } from "../globalSlice";
-import { open as _openModal } from "./Modal/modalSlice";
+import { MovieLink } from "./MovieLink";
 
 interface TopMoviesProps {
   movieData: Array<any>;
 }
 
 export const TopMovies: FC<TopMoviesProps> = ({ movieData }) => {
-  const dispatch = useDispatch();
-
-  const openModal = (modalName: string, movieId: number) => (event: any) => {
-    event.preventDefault();
-
-    dispatch(setCurrentMovieId(movieId));
-    dispatch(_openModal(modalName));
-  };
-
   return (
     <ul className="top-list">
+      {!movieData?.length && <li className="top-list-item empty">Empty</li>}
       {movieData &&
         movieData.slice(0, 10).map((movie: { id: number; title: string }) => (
           <li key={movie.id} className="top-list-item">
-            <button className="movie-link" onClick={openModal("movie-details", movie.id)}>
-              {movie.title}
-            </button>
+            <MovieLink type="simple" localItem={movie} modalName="movie-details" />
           </li>
         ))}
     </ul>
